c5de739d10c06c24a15b8643dd2059e89da5bf72
Guillermo Ramos <memo33101@gmail.com>
2019-05-19 19:35:01 -0600

    Added button, and listener, that creates a new MeaningfulTask

--- app/src/main/java/com/guillermoramos/meaningfulday/controller/AddActivity.java
+++ app/src/main/java/com/guillermoramos/meaningfulday/controller/AddActivity.java
@@ -6,25 +6,55 @@ import android.view.View;
 import android.os.Bundle;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.guillermoramos.meaningfulday.R;
+import com.guillermoramos.meaningfulday.model.MeaningfulTask;
+
+import java.util.ArrayList;
 
 
 public class AddActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
+    private Button submitButton;
+    private MeaningfulTask newMeaningfulTask;
+
+    private ArrayList<MeaningfulTask> taskArrayList = new ArrayList<MeaningfulTask>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
-        Spinner categorySpinner = findViewById(R.id.task_categories);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+
+        final Spinner categorySpinner = findViewById(R.id.task_categories);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categorySpinner.setAdapter(adapter);
         categorySpinner.setOnItemSelectedListener(this);
+
+        final EditText taskInput = findViewById(R.id.textInput);
+
+        submitButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String task = taskInput.getText().toString();
+                String category = categorySpinner.getSelectedItem().toString();
+
+                newMeaningfulTask = new MeaningfulTask(task, category);
+
+                taskArrayList.add(newMeaningfulTask);
+
+            }
+        });
     }
+
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
@@ -37,4 +67,6 @@ public class AddActivity extends AppCompatActivity implements AdapterView.OnItem
     {
 
     }
+
+
 }
