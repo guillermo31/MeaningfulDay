c54b24a9449e450a74d5d67a8aa787770a740fab
Guillermo Ramos <memo33101@gmail.com>
2019-05-19 20:06:14 -0600

    Created Progress page

---
+++ app/src/main/java/com/guillermoramos/meaningfulday/controller/ProgressActivity.java
@@ -0,0 +1,17 @@
+package com.guillermoramos.meaningfulday.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.guillermoramos.meaningfulday.R;
+
+public class ProgressActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress);
+    }
+}
