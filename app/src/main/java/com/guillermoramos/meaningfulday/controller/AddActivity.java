323f831fdb598797f3cd182d1c016337ec88ad3e
Guillermo Ramos <memo33101@gmail.com>
2019-05-19 18:54:10 -0600

    Added input fields to create a MeaningfulTask object

--- app/src/main/java/com/guillermoramos/meaningfulday/controller/AddActivity.java
+++ app/src/main/java/com/guillermoramos/meaningfulday/controller/AddActivity.java
@@ -4,15 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.guillermoramos.meaningfulday.R;
 
 
-public class AddActivity extends AppCompatActivity {
+public class AddActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        Spinner categorySpinner = findViewById(R.id.task_categories);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(adapter);
+        categorySpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+    {
+        String category = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+
     }
 }
