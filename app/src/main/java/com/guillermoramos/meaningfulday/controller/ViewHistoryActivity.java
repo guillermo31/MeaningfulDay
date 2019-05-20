caca6910c8b09f56e5dc55189f2eeffd1ea2a95f
Guillermo Ramos <memo33101@gmail.com>
2019-05-20 07:50:00 -0600

    Created View History activity

---
+++ app/src/main/java/com/guillermoramos/meaningfulday/controller/ViewHistoryActivity.java
@@ -0,0 +1,15 @@
+package com.guillermoramos.meaningfulday.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.guillermoramos.meaningfulday.R;
+
+public class ViewHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_history);
+    }
+}
